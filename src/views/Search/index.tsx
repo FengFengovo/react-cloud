8e1a1744d8fae2b88504c576f9f10cdf58c7aeb9
风风 <2083147939@qq.com>
2024-11-19 21:15:09 +0800

    1

--- src/views/Search/index.tsx
+++ src/views/Search/index.tsx
@@ -1,8 +1,9 @@
 import FSongList from "@/components/FSongList";
 import {useSearchParams} from "react-router-dom";
-
+import useGetList from "@/components/FSongList/useGetList.ts";
 const Search =()=>{
     const [searchParams]= useSearchParams()
+    const {playList,isLoading} = useGetList()
     //从路由参数中提取出关键字
     const key = searchParams.get('key');
     // 只在有 key 的时候才调用 useGetList
@@ -11,7 +12,7 @@ const Search =()=>{
 
             <div className={'text-white w-90% m-auto pl-12px py-30px text-30px'}>{key}的相关搜索如下</div>
             <div className={'h-full '}>
-                <FSongList />
+                <FSongList playList={playList} isLoading={isLoading}/>
             </div>
         </div>
     )
